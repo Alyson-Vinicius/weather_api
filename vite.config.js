0a5299df183eda1c00a41ea2cb4477dd8f9cdbdb
Alyson-Vinicius <avgs1@discente.ifpe.edu.br>
2025-05-11 22:23:54 -0300

    feat: add PWA support with custom service worker configuration

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      srcDir: 'public',           // Pasta onde está seu custom-sw.js
+      filename: 'custom-sw.js',   // Nome do seu service worker customizado
+      strategies: 'injectManifest', // Usa seu próprio service worker e injeta o manifest
+      registerType: 'autoUpdate', // Atualiza automaticamente o SW
+      manifest: {
+        name: "Weather API",
+        short_name: "Weather",
+        theme_color: "#2196f3",
+        background_color: "#2196f3",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        description: "Aplicação para consultar a temperatura de cidades.",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
+    })
+  ]
 })
